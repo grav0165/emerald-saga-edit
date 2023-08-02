eee7ea0d632eb5fbf82d1b913f29e85ef000e7c4
Emma Stout <emma@primeacademy.io>
2023-08-02 11:46:52 -0500

    main working with multiple fields

--- src/components/StudentForm/StudentForm.jsx
+++ src/components/StudentForm/StudentForm.jsx
@@ -1,36 +1,47 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 /*
 YOU SHOULDNT NEED TO MODIFY ANYTHING IN THIS FILE
 */
 
-function StudentForm(props) {
-    
-    const [student, setStudent] = useState({ githubName: '' });
+function StudentForm() {
+    const dispatch = useDispatch();
+
+    const [student, setStudent] = useState({ cohort: '', first_name: '', last_name: '', github_name: '' });
 
     // Called when the submit button is pressed
     const handleSubmit = (event) => {
         event.preventDefault();
-        props.addStudent(student);
-        clearStudentFields();
-    }
-
-    // Clear fields of the form by reseting the user
-    const clearStudentFields = () => {
-        setStudent({ githubName: '' });
+        // Dispatch an action to trigger the addStudent saga
+        dispatch({ type: 'ADD_STUDENT', payload: student })
+        // Clear fields
+        setStudent({ cohort: '', first_name: '', last_name: '', github_name: '' });
     }
 
 
     return (
         <form onSubmit={handleSubmit}>
-            <input onChange={(event) => setStudent({githubName: event.target.value})} 
-                    placeholder="GitHub username" 
-                    value={student.githubName}
-                    />
+            <input onChange={(event) => setStudent({ ...student, cohort: event.target.value })}
+                placeholder="Cohort"
+                value={student.cohort}
+            />
+            <input onChange={(event) => setStudent({ ...student, first_name: event.target.value })}
+                placeholder="First name"
+                value={student.firstName}
+            />
+            <input onChange={(event) => setStudent({ ...student, last_name: event.target.value })}
+                placeholder="Last name"
+                value={student.lastName}
+            />
+            <input onChange={(event) => setStudent({ ...student, github_name: event.target.value })}
+                placeholder="GitHub username"
+                value={student.githubName}
+            />
             <input type="submit" value="Add Student" />
         </form>
     );
-    
+
 }
 
 
